14f0c0c38dfb2badf3278255cbe21dbbe34eed2b
JunCheng <catcat1234567@gmail.com>
2020-07-14 23:03:31 +0800

    初步版本完成，完成基礎對話功能

---
+++ RPGFunctions/Assets/RpgDialogue Scripts/DialogueFunction.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueFunction : MonoBehaviour
+{
+    public RawImage characterImg;                                               //For Center Mode
+
+    public RawImage characterLImg;                                              //For   L/R  Mode
+
+    public RawImage characterRImg;                                              //For   L/R  Mode
+
+    public GameObject DialogueBox;                                              //The Dialogue Box
+
+    public Text DialogueBoxText;                                                //The Dialogue Box Text
+
+    private int dialogueNumber = 0;                                             //Record Current Dialogue Number
+
+    private int partNumber = 0;                                                 //Record Current Part Number(Change it when switch character)
+
+    private DialogueManager.DialougDataBase dialoguedata;                       //Get dialoguedata for manager(current drama script)
+
+    private void Start()                                                        
+    {
+        dialoguedata = DialogueManager._dialogueManager.DialougDB;              //Same as the up line.
+
+        AdjustCharacterPosition(1);
+        AdjustCharacterPosition(0);
+
+        LoadCharacterDialogue();
+    }
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+            dialogueNumber++; LoadCharacterDialogue(); Debug.Log("P:"+partNumber);
+        }  //Click to show the next Dialogue
+
+    }
+
+    Texture2D LoadCharacterImage(string characterName)
+    {
+        #region LoadImage
+        //characterLImg.texture = null;
+        //characterRImg.texture = null;
+
+        FileStream fileStream = new FileStream(Application.dataPath + "/StreamingFiles/CharacterPictures/" + characterName + ".png", FileMode.Open,FileAccess.Read);
+        fileStream.Seek(0, SeekOrigin.Begin);
+        byte[] bytes = new byte[fileStream.Length];
+        fileStream.Read(bytes, 0, (int)fileStream.Length);
+        fileStream.Close();
+        fileStream.Dispose();
+        fileStream = null;
+        Texture2D texture = new Texture2D(600, 800);
+        texture.LoadImage(bytes);
+
+        return texture;
+        #endregion
+    }
+
+    void AdjustCharacterPosition(int PNumber)
+    {
+        #region AdjustCharacterPosition
+        if (dialoguedata.list[PNumber].talkType == DialogueManager.talkingType.LeftRightDouble)
+        {
+            if (dialoguedata.list[PNumber].talkPos == DialogueManager.talkingPos.Left)
+            {
+                characterLImg.texture = LoadCharacterImage(dialoguedata.list[PNumber].characterName);
+                characterRImg.GetComponent<RawImage>().color = new Color32(75, 75, 75, 255);
+                characterLImg.GetComponent<RawImage>().color = new Color32(255, 255, 255, 255);
+            }
+
+            else if (dialoguedata.list[PNumber].talkPos == DialogueManager.talkingPos.Right)
+            {
+                characterRImg.texture = LoadCharacterImage(dialoguedata.list[PNumber].characterName);
+                characterLImg.GetComponent<RawImage>().color = new Color32(75, 75, 75, 255);
+                characterRImg.GetComponent<RawImage>().color = new Color32(255, 255, 255, 255);
+            }
+        }
+        #endregion
+    }
+
+    void LoadCharacterDialogue()
+    {
+        if (partNumber+1> dialoguedata.list.Count - 1&& dialogueNumber > dialoguedata.list[partNumber].characterDialoug.Count - 1)
+        {
+            Debug.Log("The part is ended");
+            DialogueBox.SetActive(false);
+            return;
+        }
+
+        if (dialogueNumber > dialoguedata.list[partNumber].characterDialoug.Count - 1)
+        {
+            partNumber++;
+            Debug.Log("The character's Dialogue is ended");
+            AdjustCharacterPosition(partNumber);
+            dialogueNumber = 0;
+        }
+
+        DialogueBoxText.text = dialoguedata.list[partNumber].characterDialoug[dialogueNumber];
+        //Debug.Log(partNumber);
+    }
+}
---
+++ RPGFunctions/Assets/RpgDialogue Scripts/DialogueManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager _dialogueManager;
+
+    private void Awake()
+    {
+        _dialogueManager = this;
+        _dialogueManager.LoadDrama("Dialoug_data");
+    }
+    //-----------------------------------------------------------
+    public DialougDataBase DialougDB;
+
+    public void SaveDialogues()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(DialougDataBase));
+        FileStream fileStream = new FileStream(Application.dataPath+"/StreamingFiles/XML/Dialoug_data.xml",FileMode.Create);
+        serializer.Serialize(fileStream, DialougDB);
+        fileStream.Close();
+    }
+
+    public void LoadDrama(string dramaScriptName)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(DialougDataBase));
+        FileStream fileStream = new FileStream(Application.dataPath + "/StreamingFiles/XML/"+ dramaScriptName + ".xml", FileMode.Open);
+        DialougDB = serializer.Deserialize(fileStream) as DialougDataBase;
+        fileStream.Close();
+
+        Debug.Log(DialougDB.list[0].characterName);
+        Debug.Log(DialougDB.list[1].characterName);
+    }
+
+    [System.Serializable]
+    public class DialougEntry
+    {
+        public string characterName;
+        public string soundName;
+        public talkingType talkType;
+        public talkingPos talkPos;
+        public List<string> characterDialoug = new List<string>();
+    }
+    [System.Serializable]
+    public class DialougDataBase
+    {
+        public List<DialougEntry> list = new List<DialougEntry>();
+    }
+
+    public enum talkingType
+    {
+       CenterOne,
+       LeftRightDouble,
+    }
+
+    public enum talkingPos
+    {
+        Left,
+        Right,
+    }
+}
